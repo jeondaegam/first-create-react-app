07a8deded5c3d3acd9aaa49ec8afbb9ae76398f7
jeondaegam <jeondaegam@naver.com>
2022-02-03 16:26:32 +0900

    #6.1 useEffect - function을 최초 한번만 실행한다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Button from "./Button";
 import styles from "./App.module.css"
+import { useEffect, useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => {setCounter((prev) => prev + 1);}
+  
+  console.log("I run all the time");
+
+  const iRunOnlyOnce = () => {
+    console.log("CALL THE API,,,");
+  }
+
+  // useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("CALL THE API ...");
+  }, [])
+  
+  // <Button onClick={onClick} text="hey your button here"/>
   return (
-    <div><h1 className={styles.title}>Welcome back!</h1>
-    <Button text="hey your button here" />
+    <div><h1 className={styles.title} >{counter}</h1>
+    <button onClick={onClick}>Click me</button>
     </div>
   );
 }
