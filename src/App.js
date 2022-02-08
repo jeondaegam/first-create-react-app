2a370f4cfe4a86d260c8a5ec1a47d52443af844b
jeondaegam <jeondaegam@naver.com>
2022-02-08 19:31:46 +0900

    #6.4 Cleanup
    - useEffect를 이용해 component가 destroy될 때 function을 실행할 수 있다.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
-import Button from "./Button";
 import styles from "./App.module.css"
 import { useEffect, useState } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => {setCounter((prev) => prev + 1);}
-  const onChange = (event) => {setKeyword(event.target.value);} 
-  // onChange로 keyword의 변화를 감지하면, value를 수정한다.
 
-  // console.log("I run all the time");  
-  // useEffect(iRunOnlyOnce, []);
-
-  useEffect( () => {
-    console.log("I run only once")}, []
-  );
-
-  useEffect(() => {}, []);
-  // keyword가 변화할 때만 코드를 실행한다.
-  useEffect(()=> {
-    console.log("I run when 'keyword' changes.");
-    console.log('keyword: ', keyword);
-  }, [keyword]);
-
-  useEffect(()=> {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-
-  useEffect(() => {
-    console.log("I run when keyword & counter change")
-  }, [keyword, counter]);
-
-  // 이것이 빈값일땐 1번만 실행되는 이유이다.
-  // 빈 값일 경우 react가 감지할 값이 없기때문에 처음 1번만 실행된다.
+function Hello() {
   
-  // <Button onClick={onClick} text="hey your button here"/>
+  useEffect(()=> {
+    console.log("hi :)");
+    return () => console.log("bye :("); 
+    // class가 메모리를 반납하며 return하는것과 비슷한 느낌..
+    // <h1> 컴포넌트가 사라질 때 실행한다.
+  },[]);
+
+  return <h1>Hello</h1>; 
+}
+
+
+function App() {
+  const [showing, setShowing] = useState(true);
+  const onClick = () => setShowing(prev => !prev)
+
   return (
     <div>
-    <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
-    <h1 className={styles.title}>{counter}</h1>
-    <button onClick={onClick}>Click me</button>
-    <h3>{keyword}</h3>
+      {showing ? <Hello /> : null}
+    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
