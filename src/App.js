2ca1676cb772bf4104b10aed72e948158710cf59
jeondaegam <jeondaegam@naver.com>
2022-02-09 07:28:28 +0900

    #7.0 Todo List part 1 : ...array
    - ...을 사용하여 array에 새로운 값을 추가한다.
    - <input>, <form> 사용하기
    - submit  event prevent

--- src/App.js
+++ src/App.js
@@ -16,13 +16,34 @@ function Hello() {
 
 
 function App() {
-  const [showing, setShowing] = useState(true);
-  const onClick = () => setShowing(prev => !prev)
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo==="") {
+      return; // toDo가 mepty면 이 함수를 실행시키지 않는다
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo(""); // 키워드 입력 후 submit하면 input창을 비워준다.
+  };
+    console.log(toDos);
+
+    // setToDos(function(currentArray)){ 위와 똑같은 기능을 한다.
+    //   return ;
+    // }
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>MY TO DOS ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+      <input onChange={onChange} 
+      value={toDo}
+      text="text"
+      placeholder="Write your to do!"
+      ></input>
+      <button>Add To Do</button>
+      </form>
     </div>
   );
 }
